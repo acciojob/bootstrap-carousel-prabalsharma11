695f2a5362efec414f6b3194fb73ce00a78c8f03
Prabal Raj Sharma <109718530+prabalsharma11@users.noreply.github.com>
2024-11-08 14:47:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your code here
+cy.get('.button-class') // If this matches multiple elements, make it more specific.
+cy.get('.button-class').eq(0).click(); // Clicks only the first element.
+cy.get('.button-class').click({ multiple: true });
+cy.get('.button-class').should('have.length', 1);
+cy.get('.parent-class .child-class') // Use parent-child relationships.
+cy.get('.button-class').debug();
